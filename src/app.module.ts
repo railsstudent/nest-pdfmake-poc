aa28f16c8d54967535552f75cc2a80b273ab41d9
Connie Leung <cleung334@gmail.com>
2022-04-17 02:42:17 +0800

    chore: remove HeaderResolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { HeaderResolver, I18nModule } from 'nestjs-i18n'
+import { I18nModule } from 'nestjs-i18n'
 import * as path from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -15,7 +15,6 @@ import { InvoiceModule } from './invoice'
         path: path.join(__dirname, '/i18n/'),
         watch: true,
       },
-      resolvers: [new HeaderResolver(['language'])],
     }),
     InvoiceModule,
   ],
